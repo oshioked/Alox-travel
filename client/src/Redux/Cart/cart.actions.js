7e49ad48b0df12b266efe37cf3cac558f55d5d63
daan <danieloshos3@gmail.com>
2023-09-27 19:10:26 +0100

    remove console.log

--- client/src/Redux/Cart/cart.actions.js
+++ client/src/Redux/Cart/cart.actions.js
@@ -1,32 +1,31 @@
 export const cartTypes = {
-    TOGGLE_CART_DISPLAY: 'TOGGLE_CART_DISPLAY',
-    ADD_ITEM_TO_CART: 'ADD_ITEM_TO_CART',
-    CLEAR_ITEM: 'CLEAR_ITEM',
-    REDUCE_ITEM: 'REDUCE_ITEM'
+  TOGGLE_CART_DISPLAY: "TOGGLE_CART_DISPLAY",
+  ADD_ITEM_TO_CART: "ADD_ITEM_TO_CART",
+  CLEAR_ITEM: "CLEAR_ITEM",
+  REDUCE_ITEM: "REDUCE_ITEM",
 }
 
-export const ToggleCartDisplay = () =>{
-    return({
-        type: cartTypes.TOGGLE_CART_DISPLAY
-    })
-}
-    
-export const AddItemToCart = (item) =>{
-    console.log(item)
-    return({
-        type: cartTypes.ADD_ITEM_TO_CART,
-        payload: item
-    })
+export const ToggleCartDisplay = () => {
+  return {
+    type: cartTypes.TOGGLE_CART_DISPLAY,
+  }
 }
 
-export const clearItem = (itemId) =>{
-    return({
-        type: cartTypes.CLEAR_ITEM,
-        payload: itemId
-    })
+export const AddItemToCart = (item) => {
+  return {
+    type: cartTypes.ADD_ITEM_TO_CART,
+    payload: item,
+  }
 }
 
-export const reduceItemQty = (itemId) =>({
-    type: cartTypes.REDUCE_ITEM,
-    payload:itemId
+export const clearItem = (itemId) => {
+  return {
+    type: cartTypes.CLEAR_ITEM,
+    payload: itemId,
+  }
+}
+
+export const reduceItemQty = (itemId) => ({
+  type: cartTypes.REDUCE_ITEM,
+  payload: itemId,
 })
